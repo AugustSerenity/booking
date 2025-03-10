72c23d797564964c9f4c255308db6d2c5896989d
Gleb <dawgkek@mail.ru>
2025-03-10 20:40:39 +0300

    added tests with POST

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -21,10 +22,24 @@ var theTests = []struct {
 	{"home", "/", "GET", []postData{}, http.StatusOK},
 	{"about", "/about", "GET", []postData{}, http.StatusOK},
 	{"generals-quarters", "/generals-quarters", "GET", []postData{}, http.StatusOK},
-	//{"m-s", "/majors-suit", "GET", []postData{}, http.StatusOK},
+	{"m-s", "/majors-suite", "GET", []postData{}, http.StatusOK},
 	{"s-a", "/search-availability", "GET", []postData{}, http.StatusOK},
 	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
 	{"make-reservation", "/make-reservation", "GET", []postData{}, http.StatusOK},
+	{"post-search-av", "/search-availability", "POST", []postData{
+		{key: "start", value: "2025-01-01"},
+		{key: "end", value: "2025-01-11"},
+	}, http.StatusOK},
+	{"post-search-av-json", "/search-availability-json", "POST", []postData{
+		{key: "start", value: "2025-01-01"},
+		{key: "end", value: "2025-01-11"},
+	}, http.StatusOK},
+	{"post-make-reservation", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "JoJo"},
+		{key: "last_name", value: "Onimechkin"},
+		{key: "email", value: "Anime@NerYlit.com"},
+		{key: "phone", value: "123-21434-1136"},
+	}, http.StatusOK},
 }
 
 func TestNewHandlers(t *testing.T) {
@@ -44,7 +59,19 @@ func TestNewHandlers(t *testing.T) {
 				t.Errorf("for %s, expected %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 			}
 		} else {
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
 
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s, expected %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
 		}
 	}
 }
