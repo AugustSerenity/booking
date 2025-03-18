37eb87a52538ac3dcbaab6f66e0a91c0e92d6cda
Gleb <dawgkek@mail.ru>
2025-03-18 19:37:18 +0300

    update handlers postReservation + db model

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,9 @@
 package repository
 
+import "github.com/AugustSerenity/booking/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+
+	InsertReservation(res models.Reservation) error
 }
